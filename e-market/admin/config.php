3eaa2a2968b9aa1aa12dff49c9bc11691ae3bed5
OpenShift System User <58984b277628e1f7f200010e@ex-std-node541.prod.rhcloud.com>
2017-02-06 07:58:50 -0500

    Добавил opencart

---
+++ e-market/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://php54-tptp.rhcloud.com/e-market/admin/');
+define('HTTP_CATALOG', 'http://php54-tptp.rhcloud.com/e-market/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://php54-tptp.rhcloud.com/e-market/admin/');
+define('HTTPS_CATALOG', 'http://php54-tptp.rhcloud.com/e-market/');
+
+// DIR
+define('DIR_APPLICATION', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/admin/');
+define('DIR_SYSTEM', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/');
+define('DIR_IMAGE', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/image/');
+define('DIR_LANGUAGE', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/admin/language/');
+define('DIR_TEMPLATE', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/admin/view/template/');
+define('DIR_CONFIG', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/config/');
+define('DIR_CACHE', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/storage/download/');
+define('DIR_LOGS', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/storage/modification/');
+define('DIR_UPLOAD', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/system/storage/upload/');
+define('DIR_CATALOG', '/var/lib/openshift/58984b277628e1f7f200010e/app-root/runtime/repo/e-market/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.4.108.2');
+define('DB_USERNAME', 'admintyJsZrz');
+define('DB_PASSWORD', '2qPEKrJSwEBs');
+define('DB_DATABASE', 'php54');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
