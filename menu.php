ae801e978f465c566076db938f7d8eb5091e69bd
romych82 <rtemchenko@gmail.com>
2021-12-08 15:49:04 +0200

    menu.php

---
+++ menu.php
@@ -0,0 +1,64 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="lines-button x navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="lines"></span>
+
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                     <img src="logo.png" class="img-polaroid logo-custom"/>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Сервис</a>
+                    </li>
+
+
+
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle pushed-menu" data-toggle="dropdown">Каталог/Portfolio <b class="caret"></b></a>
+          <ul class="dropdown-menu navbar-toggle pushed-menu-down" data-no-collapse="true">
+            <li><a class="page-scroll" href="#portfolio">Полное Portfolio</a></li>
+             <li class="divider"></li>
+            <li><a href="posm_kassa.html">Прикассовые стойки</a></li>
+            <li><a href="posm_stoyka.html">Рекламно-Торговые стеллажи</a></li>
+            <li><a href="posm_stellazh.html">Стационарные стеллажи</a></li>
+            <li><a href="lightbox.html">Рекламные вывески</a></li>
+            <li><a href="posm_strip_holder.html">Пластиковые держатели</a></li>
+            <li><a href="posm_wire_display.html">Проволочные дисплеи</a></li>
+            <li><a href="posm_wire_stoyka.html">Облегченные сетчатые стойки</a></li>
+            <li><a href="posm_acryl.html">Изделия из прозрачного пластика</a></li>
+            <li><a href="#">Широкоформатная печать</a></li>
+            <li><a href="posm_shtender.html">Штендеры. Х-баннер. Стенды.</a></li>
+            <li><a href="#">Лазерная гравировка</a></li>
+          </ul>
+        </li>
+
+
+
+                                        <!--
+                                        <li>
+                                            <a class="page-scroll" href="/e-market/index.php">Интернет магазин</a>
+                                        </li>-->
+                    <li>
+                        <a class="page-scroll" href="#team">Команда</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
