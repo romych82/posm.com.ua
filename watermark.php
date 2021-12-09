4fa35156e55d82e1c26d31b8de3a728e947dbc4b
romych82 <rtemchenko@gmail.com>
2021-12-09 15:47:10 +0200

    Test

--- watermark.php
+++ watermark.php
@@ -1,24 +1,11 @@
 <?php
-/*
-	PHP Watermark
-	Author Alferov D. WDDA
-	https://github.com/wdda/watermark
-*/
 
-
-
-
-$dir = 'cache';
-if(!is_dir($dir)) mkdir($dir);
 
 //Путь до файла с оригинальным изображением
 $path = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'];
 $nameImage = end(explode('/', $_SERVER['REQUEST_URI'])); //Имя изображения
 $nameImageId = md5($path) . '_' . $nameImage; //Имя изображения в кеше
 
-echo $path;
-echo $nameImage;
-echo $nameImageId;
 
 newImage();
 
