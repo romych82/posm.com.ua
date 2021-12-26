6ab59bd3a261f9cee77303937ad2ecb77e7707b2
romych82 <rtemchenko@gmail.com>
2021-12-26 14:56:35 +0200

    Test

---
+++ Test/index3.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<html lang="ru" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Марк Отто, Джейкоб Торнтон и контрибьюторы Bootstrap">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Шаблон обложки · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.su/docs/5.0/examples/cover/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+
+
+
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+    <link href="css/agency.css" rel="stylesheet">
+</head>
+<body class="d-flex h-100 text-center text-white bg-dark intro-image1" >
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+        <div>
+            <h3 class="float-md-start mb-0">Обложка</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+                <a class="nav-link active" aria-current="page" href="#">Главная</a>
+                <a class="nav-link" href="#">Особенности</a>
+                <a class="nav-link" href="#">Контакты</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="px-3">
+        <h1>Обложка Вашей страницы.</h1>
+        <p class="lead">Обложка - это одностраничный шаблон для создания простых и красивых домашних страниц. Загрузите, отредактируйте текст и добавьте свою собственную полноэкранную фоновую фотографию, чтобы сделать ее своей собственной.</p>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Узнать больше</a>
+        </p>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+        <p>Шаблон обложки для <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, от <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+    </footer>
+</div>
+
+
+
+</body>
+</html>
