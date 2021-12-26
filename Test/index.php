893193811545f123b1a86d3a353326aecf21cba9
romych82 <rtemchenko@gmail.com>
2021-12-26 17:28:00 +0200

    Test

--- Test/index.php
+++ Test/index.php
@@ -113,7 +113,7 @@
         </div>
 
         <div class="row">
-            <div class="col-md-4 portfolio-item">
+            <div class="col portfolio-item">
                 <a href="posm_kassa.php" class="portfolio-link"">
                 <div class="portfolio-hover">
                     <div class="portfolio-hover-content">
@@ -127,21 +127,42 @@
                 </a>
                 <div class="portfolio-caption">
                     <h4>Прикассовые стойки. Стойки для касс самообслуживания</h4>
-
-
-
                 </div>
-
-
-
-
-
-
-
-
             </div>
 
+            <div class="col portfolio-item">
+                <a href="posm_kassa.php" class="portfolio-link"">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                        УЗНАТЬ БОЛЬШЕ
+                    </div>
+                </div>
 
+                <img data-original="img/portfolio/posm_kassa.jpg" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC" class="lazy img-responsive" alt="" width="400" height="289">
+
+
+                </a>
+                <div class="portfolio-caption">
+                    <h4>Прикассовые стойки. Стойки для касс самообслуживания</h4>
+                </div>
+            </div>
+
+            <div class="col portfolio-item">
+                <a href="posm_kassa.php" class="portfolio-link"">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                        УЗНАТЬ БОЛЬШЕ
+                    </div>
+                </div>
+
+                <img data-original="img/portfolio/posm_kassa.jpg" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC" class="lazy img-responsive" alt="" width="400" height="289">
+
+
+                </a>
+                <div class="portfolio-caption">
+                    <h4>Прикассовые стойки. Стойки для касс самообслуживания</h4>
+                </div>
+            </div>
 
 
         </div>
---
+++ Test/index5.php
@@ -0,0 +1,114 @@
+
+<!doctype html>
+<html lang="ru" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Марк Отто, Джейкоб Торнтон и контрибьюторы Bootstrap">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Шаблон обложки · Bootstrap v5.0</title>
+
+    <link rel="canonical" href="https://getbootstrap.su/docs/5.0/examples/cover/">
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+    <link href="css/agency.css" rel="stylesheet">
+
+</head>
+
+
+<body
+
+<main>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col portfolio-item">
+                <a href="posm_kassa.php" class="portfolio-link"">
+                <div class="portfolio-hover">
+                    <div class="portfolio-hover-content">
+                        УЗНАТЬ БОЛЬШЕ
+                    </div>
+                </div>
+
+                <img data-original="img/portfolio/posm_kassa.jpg" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC" class="lazy img-responsive" alt="" width="400" height="289">
+
+
+                </a>
+                <div class="portfolio-caption">
+                    <h4>Прикассовые стойки. Стойки для касс самообслуживания</h4>
+                </div>
+            </div>
+
+
+            <div class="col"><img src="img/portfolio/posm_kassa.jpg" width="400" height="289"></div>
+            <div class="col"><img src="img/portfolio/posm_kassa.jpg" width="400" height="289"></div>
+
+        </div>
+
+
+        <div class="row">
+            <div class="col "><img src="img/portfolio/posm_kassa.jpg" width="400" height="289"></div>
+            <div class="col"><img src="img/portfolio/posm_kassa.jpg" width="400" height="289"></div>
+            <div class="col"><img src="img/portfolio/posm_kassa.jpg" width="400" height="289"></div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+</main>
+
+
+
+<script src="css/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+
+
+<!-- jQuery Version 1.11.0 -->
+<script src="js/jquery-1.11.0.js"></script>
+
+<!-- jquery.lazyload Version -->
+<script type="text/javascript" src="js/jquery.lazyload.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/cbpAnimatedHeader.js"></script>
+<script src="js/cbpAnimatedTex.js"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/agency.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="js/scroll_load.js"></script>
+<script type="text/javascript" src="js/mootools-1.2.4-core-yc.js"></script>
+<script type="text/javascript" src="js/lazy-load.js"></script>
+
+<script type="text/javascript" charset="utf-8">
+    $(function() {
+        $("img.lazy").show().lazyload({
+            effect : "fadeIn"
+        });
+    });
+</script>
+
+
+</body>
+</html>
