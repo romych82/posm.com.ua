05cd44a8e239c8ac911058836d13bc6e876941f0
romych82 <rtemchenko@gmail.com>
2022-01-03 13:19:24 +0200

    Test

--- cover.php
+++ cover.php
@@ -25,6 +25,20 @@
     <!-- Custom styles for this template -->
     <link href="css/cover.css" rel="stylesheet">
     <link href="css/agency2.css" rel="stylesheet">
+
+    <!-- Google Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-56641214-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+    <!-- Google Analytics -->
+
 </head>
 <body class="d-flex h-100 text-center text-white bg-dark intro-image1" >
 
